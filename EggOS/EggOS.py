ee3499f9809acba41f38aed90b27dbd8305392e3
Simon <47757232+DiamondChicken0@users.noreply.github.com>
2022-10-14 12:11:29 -0400

    trying icon stuff

--- EggOS/EggOS.py
+++ EggOS/EggOS.py
@@ -1,1 +1,11 @@
-crash()
+from tkinter import *
+import tkinter 
+
+# declare the window stuff
+window = Tk()
+window.title("Python GUI App")
+window.configure(width=500, height=300)
+window.configure(bg='lightgray')
+window.mainloop()
+icon = 
+window.iconbitmap("@Egg Favicon.ico")
