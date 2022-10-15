5320f4509fc997b6362584c08be05b030ea5d83f
Simon <47757232+DiamondChicken0@users.noreply.github.com>
2022-10-15 10:18:26 -0400

    windows

--- EggOS/EggOS.py
+++ EggOS/EggOS.py
@@ -1,11 +1,19 @@
 from tkinter import *
-import tkinter 
+import tkinter as tk
+
+# declare the reg stuff
+loadingScreenWait = 3000
 
 # declare the window stuff
 window = Tk()
-window.title("Python GUI App")
-window.configure(width=500, height=300)
+window.title("EggOS")
+window.maxsize(width=1920, height= 1080)
+window.configure(width=1280, height=720)
 window.configure(bg='lightgray')
+window.iconphoto(True, tk.PhotoImage(file="Egg Favicon.png"))
+window.eval('tk::PlaceWindow . center')
+
+
 window.mainloop()
-icon = 
-window.iconbitmap("@Egg Favicon.ico")
+
+
