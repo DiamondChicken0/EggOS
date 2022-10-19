a0f3c8cb828f33bc9057a9c288f3607c34ceaecf
Simon <47757232+DiamondChicken0@users.noreply.github.com>
2022-10-19 12:25:32 -0400

    Random Stuff

--- EggOS/EggOS.py
+++ EggOS/EggOS.py
@@ -1,19 +1,48 @@
+
+from multiprocessing.connection import wait
+from operator import truediv
 from tkinter import *
 import tkinter as tk
+from time import perf_counter, sleep
+from tkinter import ttk
 
 # declare the reg stuff
-loadingScreenWait = 3000
+progressCheck = "false"
+
+
+def progDone():
+    print("done")
 
 # declare the window stuff
 window = Tk()
 window.title("EggOS")
-window.maxsize(width=1920, height= 1080)
-window.configure(width=1280, height=720)
-window.configure(bg='lightgray')
-window.iconphoto(True, tk.PhotoImage(file="Egg Favicon.png"))
+window.maxsize(width=800, height=600)
+window.minsize(width=800, height=600)
+window.configure(width=800, height=600)
+window.resizable(0,0)
+window.iconphoto(True, tk.PhotoImage(file="EggOS\EggOS Assets\Egg Favicon.png"))
 window.eval('tk::PlaceWindow . center')
 
 
-window.mainloop()
+
+# visual elements
+progressBar = ttk.Progressbar(window, orient= "horizontal", mode= "determinate", length=300)
+progressBar.pack(pady=350)
+
+timeStart = perf_counter
+sleep(1000)
+progressBar.start()
+sleep(1000)
+
+window.mainloop() 
+
+
+
+# !DO NOT REMOVE!
+# this places stuff on screen
+
+
+
+
 
 
